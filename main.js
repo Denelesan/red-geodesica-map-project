d4e0288991cebeaafc36c4a96e6948c629c3108b
denelesan <dparedesvillalobos@gmail.com>
2024-03-19 17:28:40 -0300

    Se resuelve problemas de proyección de capa WFS Red Geodésica

--- main.js
+++ main.js
@@ -5,32 +5,75 @@ function init(){
 
     // 1.- CREAR INSTANCIA DE NUESTRO OBJETO MAPA
 
-            // Crear Basemaps
+            // Crear CRS
 
-    var openStreetMap= L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                                maxZoom: 19,
-                                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                            })
+            // Obtención del CRS EPSG:25830.
+    const crs32719 = new L.Proj.CRS('EPSG:32719',
+            '+proj=utm +zone=19 +south +datum=WGS84 +units=m +no_defs', //https://spatialreference.org/ref/epsg/32719/proj4.txt
+                    {
+                        resolutions: [2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1, 0.5],
+                //Origen de servicio teselado
+                origin:[0,0]
+                    });
+
+    var crs25830 = new L.Proj.CRS('EPSG:25830',
+        '+proj=utm +zone=30 +ellps=GRS80 +units=m +no_defs', //http://spatialreference.org/ref/epsg/25830/proj4/
+        {
+            resolutions: [2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1, 0.5],
+            //Origen de servicio teselado
+            //origin:[0,0]
+        });
+
+    
+                    
+    var crsUTM84 = '+proj=utm +zone=19 +south +datum=WGS84 +units=m +no_defs +type=crs';
+    var crs3857 = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +k=1 +units=m +nadgrids=@null +wktext +no_defs +type=crs';     
+    // Crear Basemaps
+
+    var openStreetMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    })
 
     var smoothDarkMap = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
-                                minZoom: 0,
-                                maxZoom: 20,
-                                attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-                                ext: 'png'
-                            });
+        minZoom: 0,
+        maxZoom: 20,
+        
+        attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        ext: 'png'
+    });
 
     var worldImageryMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-    }); 
-
-            // Main Function
-
-    const map =  L.map("map", {
-        center: [-33.45, -70.65],
-        zoom: 10,
-        layers: smoothDarkMap
         
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    });
+
+    // Main Function
+
+    const map = new L.map("map", {
+        center: [-33.45, -70.65],
+        //center: [40.965, -5.664],
+        //center:[6307478,342560],
+        zoom: 10,
+        continuousWorld:true,
+        worlCopyJump:false,
+        layers: smoothDarkMap,
+        //crs: crs32719
     })
+
+    var layerRasterIgn = new L.tileLayer.wms("http://www.ign.es/wms-inspire/mapa-raster",
+    {
+       layers: 'mtn_rasterizado',
+       crs: crs25830,
+       format: 'image/png',
+       continuousWorld: true,
+       transparent: true,
+       attribution: '© Instituto Geográfico Nacional de España'
+})//.addTo(map)
+
+
+  
+ 
 
     L.control.scale().addTo(map);
 
@@ -70,12 +113,12 @@ function init(){
 
     //1.- Se configura variable de WFS a importar
     //http://10.13.5.43:443/geoserver/serviu/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=serviu:vw_vertices_geodesicos_vigentes&outputFormat=application%2Fjson
+
     const urlWFSRedGeodesica = 'http://10.13.5.43:443/geoserver/serviu/wfs?' +
                                 'service=wfs&' +
                                 'version=2.0.0&' +
                                 'request=GetFeature&' +
-                                'typeNames=serviu:vw_vertices_geodesicos_vigentes&' +
-                                
+                                'typeNames=serviu:vw_vertices_geodesicos_vigentes&' +                            
                                 'outputFormat=application%2Fjson'
 
     //2.- Functión para gestionar la petición y la respuesta desde un servicio WFS
@@ -102,20 +145,31 @@ function init(){
     //Función para agregar data de WFS a mapa y layer control.
     function addWFSData (WFSData, layerName){
         let WFSLayer = L.geoJSON(WFSData,{
+            coordsToLatLng: function(coords){
+                let longlat = (proj4(crsUTM84).inverse([coords[0],coords[1]]))
+                console.log(longlat[1],longlat[0])
+                return new L.LatLng(longlat[1],longlat[0])
+            },
             onEachFeature: function(feature, layer){
-                
+               
                 layer.bindPopup(feature.properties.estado)
             }
-        })
+        }).addTo(map)
         
         
         layerControl.addOverlay(WFSLayer, layerName)
-
-        console.log(WFSLayer.getBounds())
+        
         
     }
 
-   
+    
+
+    console.log("Las Coordenadas de 84 son "+proj4(crsUTM84).inverse([342560,6307478]))
+
     fetchWFSData(urlWFSRedGeodesica,"Red Geodésica")
     
+   
+    map.on("click", function(e){
+        console.log(proj4(crsUTM84,[e.latlng.lng,e.latlng.lat]))
+    })
 }
